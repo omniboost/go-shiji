34fa19acc45162af7fbfe068dfccdba78a24fb17
Bart Versluijs <bartversluijs@icloud.com>
2024-12-18 10:52:57 +0100

    feat: added support for `GetCustomers`

---
+++ get_customers.go
@@ -0,0 +1,171 @@
+package shiji
+
+import (
+	"net/http"
+	"net/url"
+
+	"github.com/omniboost/go-shiji/utils"
+)
+
+func (c *Client) NewGetCustomersRequest() GetCustomersRequest {
+	return GetCustomersRequest{
+		client:      c,
+		queryParams: c.NewGetCustomersQueryParams(),
+		pathParams:  c.NewGetCustomersPathParams(),
+		method:      http.MethodGet,
+		headers:     c.NewGetCustomersHeaders(),
+		requestBody: c.NewGetCustomersRequestBody(),
+	}
+}
+
+type GetCustomersRequest struct {
+	client      *Client
+	queryParams *GetCustomersQueryParams
+	pathParams  *GetCustomersPathParams
+	method      string
+	headers     *GetCustomersHeaders
+	requestBody GetCustomersRequestBody
+}
+
+func (c *Client) NewGetCustomersQueryParams() *GetCustomersQueryParams {
+	return &GetCustomersQueryParams{}
+}
+
+type GetCustomersQueryParams struct {
+	StatusCode string `schema:"statusCode,omitempty"`
+	Sort       string `schema:"sort,omitempty"`
+	PageNumber int32  `schema:"pageNumber,omitempty"`
+	PageSize   int32  `schema:"pageSize,omitempty"`
+	Filter     string `schema:"filter,omitempty"`
+}
+
+func (p GetCustomersQueryParams) ToURLValues() (url.Values, error) {
+	encoder := utils.NewSchemaEncoder()
+	encoder.RegisterEncoder(Date{}, utils.EncodeSchemaMarshaler)
+	encoder.RegisterEncoder(DateTime{}, utils.EncodeSchemaMarshaler)
+	encoder.RegisterEncoder(CommaSeparatedQueryParam{}, utils.EncodeSchemaMarshaler)
+	params := url.Values{}
+
+	err := encoder.Encode(p, params)
+	if err != nil {
+		return params, err
+	}
+
+	return params, nil
+}
+
+func (r *GetCustomersRequest) QueryParams() *GetCustomersQueryParams {
+	return r.queryParams
+}
+
+func (c *Client) NewGetCustomersHeaders() *GetCustomersHeaders {
+	return &GetCustomersHeaders{}
+}
+
+type GetCustomersHeaders struct{}
+
+func (r *GetCustomersRequest) Headers() *GetCustomersHeaders {
+	return r.headers
+}
+
+func (c *Client) NewGetCustomersPathParams() *GetCustomersPathParams {
+	return &GetCustomersPathParams{}
+}
+
+type GetCustomersPathParams struct {
+}
+
+func (p *GetCustomersPathParams) Params() map[string]string {
+	return map[string]string{}
+}
+
+func (r *GetCustomersRequest) PathParams() *GetCustomersPathParams {
+	return r.pathParams
+}
+
+func (r *GetCustomersRequest) PathParamsInterface() PathParams {
+	return r.pathParams
+}
+
+func (r *GetCustomersRequest) SetMethod(method string) {
+	r.method = method
+}
+
+func (r *GetCustomersRequest) Method() string {
+	return r.method
+}
+
+func (s *Client) NewGetCustomersRequestBody() GetCustomersRequestBody {
+	return GetCustomersRequestBody{}
+}
+
+type GetCustomersRequestBody struct {
+}
+
+func (r *GetCustomersRequest) RequestBody() *GetCustomersRequestBody {
+	return nil
+}
+
+func (r *GetCustomersRequest) RequestBodyInterface() interface{} {
+	return nil
+}
+
+func (r *GetCustomersRequest) SetRequestBody(body GetCustomersRequestBody) {
+	r.requestBody = body
+}
+
+func (r *GetCustomersRequest) NewResponseBody() *GetCustomersResponseBody {
+	return &GetCustomersResponseBody{}
+}
+
+type GetCustomersResponseBody PaginatedResponse[Customer]
+
+func (r *GetCustomersRequest) URL() *url.URL {
+	u := r.client.GetEndpointURL("api-gateway/configuration/v1/customers", r.PathParams())
+	return &u
+}
+
+func (r *GetCustomersRequest) Do() (GetCustomersResponseBody, error) {
+	// Create http request
+	req, err := r.client.NewRequest(nil, r)
+	if err != nil {
+		return *r.NewResponseBody(), err
+	}
+
+	// Process query parameters
+	err = utils.AddQueryParamsToRequest(r.QueryParams(), req, false)
+	if err != nil {
+		return *r.NewResponseBody(), err
+	}
+
+	responseBody := r.NewResponseBody()
+	_, err = r.client.Do(req, responseBody)
+	return *responseBody, err
+}
+
+func (r *GetCustomersRequest) All() ([]Customer, error) {
+	customers := []Customer{}
+	for {
+		resp, err := r.Do()
+		if err != nil {
+			return customers, err
+		}
+
+		// Break out of loop when no customers are found
+		if len(resp.Results) == 0 {
+			break
+		}
+
+		// Add customers to list
+		customers = append(customers, resp.Results...)
+
+		if len(customers) == resp.Paging.TotalCount {
+			break
+		}
+
+		// Increment page number
+		r.QueryParams().PageNumber = r.QueryParams().PageNumber + 1
+	}
+
+	return customers, nil
+}
--- types.go
+++ types.go
@@ -315,6 +315,44 @@ type TransactionSubGroup struct {
 	TransactionGroupID string `json:"transactionGroupId"`
 }
 
+type Customer struct {
+	ID              string `json:"id"`
+	Version         int64  `json:"version"`
+	Code            string `json:"code"`
+	Name            string `json:"name"`
+	TenantName      string `json:"tenantName"`
+	Phone           string `json:"phone"`
+	Email           string `json:"email"`
+	BusinessAddress []struct {
+		AddressLine1 string `json:"addressLine1"`
+		AddressLine2 string `json:"addressLine2"`
+		City         string `json:"city"`
+		PostalCode   string `json:"postalCode"`
+		StateCode    string `json:"stateCode"`
+		StateName    string `json:"stateName"`
+		CountryCode  string `json:"countryCode"`
+		CountryName  string `json:"countryName"`
+	} `json:"businessAddress"`
+
+	TaxID           string `json:"taxId"`
+	StatusCode      string `json:"statusCode"`
+	PropertiesCount int32  `json:"propertiesCount"`
+	NonProduction   bool   `json:"nonProduction"`
+	AuditData       struct {
+		CreatorID      string   `json:"creatorId"`
+		CreationTime   DateTime `json:"creationTime"`
+		LastUpdaterID  string   `json:"lastUpdaterId"`
+		LastUpdateTime DateTime `json:"lastUpdateTime"`
+	} `json:"auditData"`
+
+	CreatorName struct {
+		FirstName string `json:"firstName"`
+		LastName  string `json:"lastName"`
+	} `json:"creatorName"`
+
+	RegionCodes []string `json:"regionCodes"`
+}
+
 type UserDetails struct {
 	ID                        string   `json:"id"`
 	AccountLoginProcedureType string   `json:"accountLoginProcedureType"`
