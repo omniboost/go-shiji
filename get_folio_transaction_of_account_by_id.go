b41a27cb7f90448ea73fbd1a47a447738fb6e2b4
Bart Versluijs <bartversluijs@icloud.com>
2025-01-09 18:49:58 +0100

    feat: added support for `GetFolioTransactionOfAccountByID`

---
+++ get_folio_transaction_of_account_by_id.go
@@ -0,0 +1,162 @@
+package shiji
+
+import (
+	"net/http"
+	"net/url"
+
+	"github.com/omniboost/go-shiji/utils"
+)
+
+func (c *Client) NewGetFolioTransactionOfAccountByIDRequest() GetFolioTransactionOfAccountByIDRequest {
+	return GetFolioTransactionOfAccountByIDRequest{
+		client:      c,
+		queryParams: c.NewGetFolioTransactionOfAccountByIDQueryParams(),
+		pathParams:  c.NewGetFolioTransactionOfAccountByIDPathParams(),
+		method:      http.MethodGet,
+		headers:     c.NewGetFolioTransactionOfAccountByIDHeaders(),
+		requestBody: c.NewGetFolioTransactionOfAccountByIDRequestBody(),
+	}
+}
+
+type GetFolioTransactionOfAccountByIDRequest struct {
+	client      *Client
+	queryParams *GetFolioTransactionOfAccountByIDQueryParams
+	pathParams  *GetFolioTransactionOfAccountByIDPathParams
+	method      string
+	headers     *GetFolioTransactionOfAccountByIDHeaders
+	requestBody GetFolioTransactionOfAccountByIDRequestBody
+}
+
+func (c *Client) NewGetFolioTransactionOfAccountByIDQueryParams() *GetFolioTransactionOfAccountByIDQueryParams {
+	return &GetFolioTransactionOfAccountByIDQueryParams{}
+}
+
+type GetFolioTransactionOfAccountByIDQueryParams struct {
+	PageNumber int32  `schema:"pageNumber,omitempty"`
+	PageSize   int32  `schema:"pageSize,omitempty"`
+	Search     string `schema:"search,omitempty"`
+	Sort       string `schema:"sort,omitempty"`
+}
+
+func (p GetFolioTransactionOfAccountByIDQueryParams) ToURLValues() (url.Values, error) {
+	encoder := utils.NewSchemaEncoder()
+	encoder.RegisterEncoder(Date{}, utils.EncodeSchemaMarshaler)
+	encoder.RegisterEncoder(DateTime{}, utils.EncodeSchemaMarshaler)
+	encoder.RegisterEncoder(CommaSeparatedQueryParam{}, utils.EncodeSchemaMarshaler)
+	params := url.Values{}
+
+	err := encoder.Encode(p, params)
+	if err != nil {
+		return params, err
+	}
+
+	return params, nil
+}
+
+func (r *GetFolioTransactionOfAccountByIDRequest) QueryParams() *GetFolioTransactionOfAccountByIDQueryParams {
+	return r.queryParams
+}
+
+func (c *Client) NewGetFolioTransactionOfAccountByIDHeaders() *GetFolioTransactionOfAccountByIDHeaders {
+	return &GetFolioTransactionOfAccountByIDHeaders{}
+}
+
+type GetFolioTransactionOfAccountByIDHeaders struct {
+	TenantID   string `schema:"AC-Tenant-ID,omitempty"`
+	PropertyID string `schema:"AC-Property-ID,omitempty"`
+}
+
+func (r *GetFolioTransactionOfAccountByIDRequest) Headers() *GetFolioTransactionOfAccountByIDHeaders {
+	return r.headers
+}
+
+func (c *Client) NewGetFolioTransactionOfAccountByIDPathParams() *GetFolioTransactionOfAccountByIDPathParams {
+	return &GetFolioTransactionOfAccountByIDPathParams{}
+}
+
+type GetFolioTransactionOfAccountByIDPathParams struct {
+	AccountID     string `schema:"AccountID"`
+	FolioID       string `schema:"FolioID"`
+	TransactionID string `schema:"TransactionID"`
+}
+
+func (p *GetFolioTransactionOfAccountByIDPathParams) Params() map[string]string {
+	return map[string]string{
+		"accountId":     p.AccountID,
+		"folioId":       p.FolioID,
+		"transactionId": p.TransactionID,
+	}
+}
+
+func (r *GetFolioTransactionOfAccountByIDRequest) PathParams() *GetFolioTransactionOfAccountByIDPathParams {
+	return r.pathParams
+}
+
+func (r *GetFolioTransactionOfAccountByIDRequest) PathParamsInterface() PathParams {
+	return r.pathParams
+}
+
+func (r *GetFolioTransactionOfAccountByIDRequest) SetMethod(method string) {
+	r.method = method
+}
+
+func (r *GetFolioTransactionOfAccountByIDRequest) Method() string {
+	return r.method
+}
+
+func (s *Client) NewGetFolioTransactionOfAccountByIDRequestBody() GetFolioTransactionOfAccountByIDRequestBody {
+	return GetFolioTransactionOfAccountByIDRequestBody{}
+}
+
+type GetFolioTransactionOfAccountByIDRequestBody struct {
+}
+
+func (r *GetFolioTransactionOfAccountByIDRequest) RequestBody() *GetFolioTransactionOfAccountByIDRequestBody {
+	return nil
+}
+
+func (r *GetFolioTransactionOfAccountByIDRequest) RequestBodyInterface() interface{} {
+	return nil
+}
+
+func (r *GetFolioTransactionOfAccountByIDRequest) SetRequestBody(body GetFolioTransactionOfAccountByIDRequestBody) {
+	r.requestBody = body
+}
+
+func (r *GetFolioTransactionOfAccountByIDRequest) NewResponseBody() *GetFolioTransactionOfAccountByIDResponseBody {
+	return &GetFolioTransactionOfAccountByIDResponseBody{}
+}
+
+type GetFolioTransactionOfAccountByIDResponseBody FolioTransaction
+
+func (r *GetFolioTransactionOfAccountByIDRequest) URL() *url.URL {
+	u := r.client.GetEndpointURL("api-gateway/cashiering/v1/accounts/{{.accountId}}/folios/{{.folioId}}/transactions/{{.transactionId}}", r.PathParams())
+	return &u
+}
+
+func (r *GetFolioTransactionOfAccountByIDRequest) Do() (GetFolioTransactionOfAccountByIDResponseBody, error) {
+	// Create http request
+	req, err := r.client.NewRequest(nil, r)
+	if err != nil {
+		return *r.NewResponseBody(), err
+	}
+
+	// Process query parameters
+	err = utils.AddQueryParamsToRequest(r.QueryParams(), req, false)
+	if err != nil {
+		return *r.NewResponseBody(), err
+	}
+
+	// Add headers
+	if r.Headers().TenantID != "" {
+		req.Header.Add("AC-Tenant-ID", r.Headers().TenantID)
+	}
+
+	if r.Headers().PropertyID != "" {
+		req.Header.Add("AC-Property-ID", r.Headers().PropertyID)
+	}
+
+	responseBody := r.NewResponseBody()
+	_, err = r.client.Do(req, responseBody)
+	return *responseBody, err
+}
---
+++ get_folio_transaction_of_account_by_id_test.go
@@ -0,0 +1,27 @@
+package shiji_test
+
+import (
+	"encoding/json"
+	"log"
+	"testing"
+
+	_ "github.com/joho/godotenv/autoload"
+)
+
+func TestGetFolioTransactionOfAccountByID(t *testing.T) {
+	client := client()
+
+	req := client.NewGetFolioTransactionOfAccountByIDRequest()
+	req.Headers().PropertyID = "50abb838-0a4e-4bea-afb4-28523f8f8daa"
+	req.PathParams().AccountID = "3519c26e-0b0d-4020-b9b3-20acaa482763"
+	req.PathParams().FolioID = "dcd6ace7-dce6-4aeb-a7b8-3660f7b9753e"
+	req.PathParams().TransactionID = "cef19de0-d295-44df-88fd-df4d638a7872"
+
+	resp, err := req.Do()
+	if err != nil {
+		t.Error(err)
+	}
+
+	b, _ := json.MarshalIndent(resp, "", "  ")
+	log.Println(string(b))
+}
